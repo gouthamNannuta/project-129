f09b72f58819f8e8e9f3d70903dd8df0cd2de112
Goutham Nannuta <69679486+gouthamNannuta@users.noreply.github.com>
2021-09-13 17:26:19 +0530

    project-128

--- data_scraping_2.py
+++ data_scraping_2.py
@@ -39,5 +39,5 @@ for i in range(1,len(temp_list)):
 
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
 print(df2)
-
+ 
 df2.to_csv('dwarf_stars.csv')
